4b0908b691faede880af37bea46e717e1f035474
chiaraxs <chiaracardinale92@gmail.com>
2021-12-14 14:57:26 +0100

    First setup

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// 1. crea un array titoli;
+// 2. crea un array paragrafi;
+// 3. ogni volta che clicco sulla freccia right carico il titolo/paragrafo successivo;
+// 4. arrivati all'ultimo titolo/paragrafo ricomincio da campo;
